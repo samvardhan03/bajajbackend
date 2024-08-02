c76c97e134aeb8107945964b07f8edca5133a0d0
Samvardhan Singh <104402477+samvardhan03@users.noreply.github.com>
2024-08-02 18:43:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+const userID = 'samvardhan_singh_03112003'; 
+const email = 'sj7873@srmist.edu.in';
+const rollNumber = 'RA2111043020005';
+
+app.post('/bfhl', (req, res) => {
+    const { data } = req.body;
+    const numbers = data.filter(item => !isNaN(item));
+    const alphabets = data.filter(item => isNaN(item));
+    const highestAlphabet = alphabets.length > 0 ? [alphabets.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' })).pop()] : [];
+
+    res.json({
+        is_success: true,
+        user_id: userID,
+        email: email,
+        roll_number: rollNumber,
+        numbers: numbers,
+        alphabets: alphabets,
+        highest_alphabet: highestAlphabet
+    });
+});
+
+app.get('/bfhl', (req, res) => {
+    res.status(200).json({
+        operation_code: 1
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
